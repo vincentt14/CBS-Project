81d8cf8045ffad52ee18e61f1d5fec1d42bad532
Vincent <vincent14@student.umn.ac.id>
2023-07-17 16:06:24 +0700

    movies model, adding refs in firebasesingleton

--- src/models/FirebaseSingleton.ts
+++ src/models/FirebaseSingleton.ts
@@ -4,31 +4,28 @@ import { CollectionReference, Firestore, collection, getFirestore, doc, Document
 
 // https://www.typescriptlang.org/docs/handbook/enums.html#const-enums
 export const enum Endpoint {
-  users = 'users',
-  movies = 'movies',
-  packages = 'packages',
+  users = "users",
+  movies = "movies",
+  packages = "packages",
 }
 
 // https://refactoring.guru/design-patterns/singleton/typescript/example
 // https://www.typescriptlang.org/docs/handbook/2/classes.html#getters--setters
 // https://firebase.google.com/docs/firestore/data-model?hl=en&authuser=0#references
 export class FirebaseSingleton {
-
   private static auth: Auth;
 
   private static db: Firestore;
 
-  private constructor() { }
+  private constructor() {}
 
   public static get getAuth(): Auth {
-    if (!this.auth || this.auth === undefined)
-      this.auth = getAuth(app);
+    if (!this.auth || this.auth === undefined) this.auth = getAuth(app);
     return this.auth;
   }
 
   private static get getFirestore(): Firestore {
-    if (!this.db || this.db === undefined)
-      this.db = getFirestore(app);
+    if (!this.db || this.db === undefined) this.db = getFirestore(app);
     return this.db;
   }
 
@@ -36,20 +33,25 @@ export class FirebaseSingleton {
     return collection(this.getFirestore, Endpoint.users);
   }
 
-  public static usersDocRef(id: string = ''): DocumentReference {
-
+  public static usersDocRef(id: string = ""): DocumentReference {
     return doc(this.getFirestore, `${Endpoint.users}/${id}`);
   }
 
   // TODO change to DocumentReference
-  public static get moviesRef(): CollectionReference {
-
+  public static moviesCollectionRef(): CollectionReference {
     return collection(this.getFirestore, Endpoint.movies);
   }
 
+  public static moviesDocRef(id: string = ""): DocumentReference {
+    return doc(this.getFirestore, `${Endpoint.movies}/${id}`);
+  }
+
   // TODO change to DocumentReference
-  public static get packagesRef(): CollectionReference {
+  public static packagesCollectionRef(): CollectionReference {
     return collection(this.getFirestore, Endpoint.packages);
   }
 
+  public static packagesDocRef(id: string = ""): DocumentReference {
+    return doc(this.getFirestore, `${Endpoint.packages}/${id}`);
+  }
 }
--- src/models/Movies.ts
+++ src/models/Movies.ts
@@ -1,4 +1,6 @@
+import { addDoc, deleteDoc, updateDoc } from "firebase/firestore";
 import { RoomPackages } from "./RoomPackage";
+import { FirebaseSingleton } from "./FirebaseSingleton";
 
 export class Movies {
   constructor(
@@ -11,5 +13,60 @@ export class Movies {
     public cinema: RoomPackages,
   ){}
 
-  
+  static createMovie = async (title: string, synopsis: string, playingTime: Date, duration: number, genre: string) => {
+    try{
+      const ref = FirebaseSingleton.moviesCollectionRef;
+      await addDoc(ref, {
+        title, 
+        synopsis, 
+        playingTime, 
+        duration, 
+        genre,
+      });
+      return {
+        success: true,
+      }
+    }catch(error){
+      return {
+        success: false,
+        message: error,
+      }
+    }
+  }
+
+  static updateMovie = async (id: string, title: string, synopsis: string, playingTime: Date, duration: number, genre: string) => {
+    try {
+      const ref = FirebaseSingleton.moviesDocRef(id);
+      await updateDoc(ref, {
+        title, 
+        synopsis, 
+        playingTime, 
+        duration, 
+        genre,
+      });
+      return {
+        success: true,
+      }
+    } catch (error) {
+      return {
+        success: false,
+        message: error,
+      }
+    }
+  }
+
+  static deleteMovie = async (id: string) => {
+    try {
+      const ref = FirebaseSingleton.moviesDocRef(id);
+      await deleteDoc(ref);
+      return {
+        success: true,
+      }
+    } catch (error) {
+      return {
+        success: false,
+        message: error,
+      }
+    }
+  }
 }
